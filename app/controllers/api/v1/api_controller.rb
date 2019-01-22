cdc33d2968214b3c5c494a7b2f7195f77271cd76
Jean <jean-marco-10@hotmail.com>
2019-01-22 11:05:10 -0600

    Fixed rubocop errors!

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -11,6 +11,6 @@ class Api::V1::ApiController < ApplicationController
 
   # Check for valid request token and return user
   def authorize_request
-    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+    @current_user = AuthorizeApiRequest.new(request.headers).call[:user]
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   # Validation tests
+  # Validation tests
   # ensure name, email and password_digest are present before save
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
